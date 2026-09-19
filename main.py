71dbac5b4f85fb3a6225adb8dbab6a8f63ee997e
TacoTuesday <310788476+TacoToosday@users.noreply.github.com>
2026-09-19 16:38:53 -0400

    Changes to .gitignore so that no more .pyc files are tracked by git.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # Main Python file for the platformer game
 import pygame
+import os
 
+running = True
+assets = os.path.join(os.path.dirname(__file__), "assets")
+
+# Main initalization of the game (DO NOT MESS WITH THIS)
+# Must be called before any other pygame functions are called
 pygame.init()
 
+
+# Example of loading an image from the assets folder
+'''player_image = pygame.image.load(
+    os.path.join(assets, "player.png")
+).convert_alpha()'''
+
+# Creation of the game window, and player
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Pygame Platformer")
 
-running = True
 clock = pygame.time.Clock()
 
 while running:
@@ -16,6 +28,6 @@ while running:
 
     window.fill((0, 0, 0))
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(60) # Sets the FPS to 60
 
 pygame.quit()
