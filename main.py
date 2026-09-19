72b31202fbe79d7fa116dd2f0b71d3ba0bb7a609
TacoTuesday <310788476+TacoToosday@users.noreply.github.com>
2026-09-19 16:20:19 -0400

    .idea files (should be ignored by the .gitignore) main loop for the game

--- main.py
+++ main.py
@@ -2,3 +2,20 @@
 import pygame
 
 pygame.init()
+
+window = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Pygame Platformer")
+
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    window.fill((0, 0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
